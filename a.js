6f9fdc338b4298e9bd0dda3b2a46959774d8b54e
yulin <yulin@microstrategy.com>
2022-02-09 23:14:06 +0800

    during feature

--- a.js
+++ a.js
@@ -5,3 +5,5 @@ console.log('hotfix')
 console.log('hotfix 2')
 
 console.log('hotfix 3')
+
+console.log('add during feature')
--- feature.js
+++ feature.js
@@ -1,3 +1,5 @@
 console.log('this file to implement feature')
 
 console.log('stash test 1')
+
+console.log('add during feature');
